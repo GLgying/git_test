47d62f2f0727babd95eca0e53618fc3e9518dd77
GLgying <G_Lybbh@163.com>
2022-11-22 20:48:04 +0800

    git—second

--- boot-redis/src/main/java/com/xxx/mvn/Application.java
+++ boot-redis/src/main/java/com/xxx/mvn/Application.java
@@ -14,5 +14,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("----");
         SpringApplication.run(Application.class,args);
+        System.out.println("----");
     }
 }
