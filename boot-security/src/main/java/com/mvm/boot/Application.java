d1cc2b61e601ee8a8557935fbcd6ba41a64784be
GLgying <G_Lybbh@163.com>
2022-12-03 11:54:49 +0800

    git—third

--- boot-security/src/main/java/com/mvm/boot/Application.java
+++ boot-security/src/main/java/com/mvm/boot/Application.java
@@ -3,6 +3,16 @@ package com.mvm.boot;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author:TuoTuo
@@ -12,9 +22,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Slf4j
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
         log.info("====启动开始====");
-        SpringApplication.run(Application.class,args);
+        ConfigurableApplicationContext application = SpringApplication.run(Application.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        if(!StringUtils.hasLength(path)){
+            path = "";
+        }
         log.info("====启动完成====");
+        log.info("\n---------------------------------------------------------------------------------------------------\n\t" +
+                "Application chat-service is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:" + port + path + "\n\t" +
+                "External: \thttp://" + ip + ":" + port + path + "\n\t" +
+//                "Swagger文档: \thttp://" + ip + ":" + port + path + "doc.html\n" +
+                "----------------------------------------------------------------------------------------------------");
     }
 }
